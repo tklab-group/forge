cb22720ff46b965445d7ad5d94935d2b49362f74
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-10-05 15:03:52 +0900

    wip: RUN命令に必要そうな型の書き出し

--- moldfile/parser/run.go
+++ moldfile/parser/run.go
@@ -1,1 +1,26 @@
 package parser
+
+type runStatement struct {
+	cmds []cmdInstruction
+}
+
+type cmdInstruction interface{} // TODO: Rename
+
+type cmdSeparator struct{}
+
+type cmd struct{}
+
+type cmdNode interface{}
+
+type cmdName struct{}
+
+type cmdOption struct{}
+
+type cmdArg struct{}
+
+type cmdInnerComment struct{}
+
+type cmdInnerBackSlash struct{}
+
+// cmdInnerSpace includes space, \r and \t
+type cmdInnerSpace struct{}
