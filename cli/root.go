2f6b37f4eb49647cda8098e3bd8040bd3ee0bd3a
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-11-13 16:23:37 +0900

    v0.0.3

--- cli/root.go
+++ cli/root.go
@@ -22,7 +22,7 @@ func newRootCmd(config config.Config) *cobra.Command {
 		Short:        "", // TODO
 		Long:         "", // TODO,
 		SilenceUsage: true,
-		Version:      "0.0.2",
+		Version:      "v0.0.3",
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			err := settingLog(logLevel)
 			if err != nil {
