6b3610a468a00cd5c38909d93826093acae424cf
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2024-02-20 13:14:59 +0900

    go installでの取得時にバージョン情報が付与されるようにした

--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"os"
+	"runtime/debug"
 
 	"github.com/tklab-group/forge/cli"
 )
 
-var version = "No version provided"
+var version string
 
 func main() {
+	// Version setting for installing with `go install`
+	if version == "" {
+		buildInfo, ok := debug.ReadBuildInfo()
+		if ok {
+			version = buildInfo.Main.Version
+		} else {
+			version = "No version provided"
+		}
+	}
+
 	cli.SetVersion(version)
 
 	err := cli.Execute()
